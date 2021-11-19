429c48ae457c3b5ab7ea0b0ae71397c48a6fab56
Max Kubik <50140834+mkubdev@users.noreply.github.com>
2021-11-19 14:56:05 +0100

    Update pages/_app.js
    thanks codacy
    
    Co-authored-by: Krish Gupta <krishguptadev@outlook.com>

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,4 @@
-import "tailwindcss/tailwind.css";
+import 'tailwindcss/tailwind.css';
 import "../styles/global.css";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
