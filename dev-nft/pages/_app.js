9dbe913b4e7bbca94178dc166edd6d50d9b2d073
Maxime TFT <mkubik@tftlabs.com>
2021-11-19 14:41:17 +0100

    🔧 init the base project with create-next-app + tailwindcss configuration

---
+++ dev-nft/pages/_app.js
@@ -0,0 +1,26 @@
+import "tailwindcss/tailwind.css";
+import "../styles/global.css";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+import { useEffect } from "react";
+import api from "../lib/appwrite";
+
+function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    api
+      .createSession()
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+  return (
+    <div>
+      <Navbar />
+      <Component {...pageProps} />
+      <Footer />
+    </div>
+  );
+}
+
+export default MyApp;
---
+++ dev-nft/tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  mode: 'jit',
+  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    animation: ["responsive", "motion-safe", "motion-reduce"],
+    extend: {},
+  },
+  plugins: [],
+}
