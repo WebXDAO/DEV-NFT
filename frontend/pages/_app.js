e3c6bc94456a9aab261f041a82f509551b5f4b20
Maxime TFT <mkubik@tftlabs.com>
2021-11-19 16:07:45 +0100

    🔑 Update the _app.js to init the appwrite SDK on startup (missing credentials yet)

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,6 +1,19 @@
 import 'tailwindcss/tailwind.css'
+import api from "../lib/appwrite";
+import { useEffect } from "react";
 
 function MyApp({ Component, pageProps }) {
+
+  // Appwrite connection
+  useEffect(() => {
+    api
+      .createSession()
+      .then((response) => {
+        console.log(response)
+      })
+      .catch((err) => console.log(err))
+  }, [])
+
   return <Component {...pageProps} />
 }
 
