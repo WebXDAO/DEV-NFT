d2c54ada12b9bc9a207997894d744daa9c2f7b92
Maxime TFT <mkubik@tftlabs.com>
2021-11-23 17:05:38 +0100

    🔧 add next-auth library + github OAuth provider

---
+++ pages/api/auth/[...nextauth].js
@@ -0,0 +1,15 @@
+import NextAuth from 'next-auth'
+import Providers from 'next-auth/providers'
+
+const options = {
+  providers: [
+    Providers.GitHub({
+      clientId: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET
+    })
+  ],
+  // Optional SQL or MongoDB database to persist users
+  // database: process.env.DATABASE_URL
+}
+
+export default (req, res) => NextAuth(req, res, options);
