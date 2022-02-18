169b3da8295e61f9665250825b61e9ad2d5e4cff
Maxime TFT <mkubik@tftlabs.com>
2022-02-18 14:10:41 +0100

    add: skeleton page to store new components

---
+++ pages/landingNew.js
@@ -0,0 +1,17 @@
+const landingNew = () => {
+  return (
+    <div className="bg-white">
+      <div className="relative overflow-hidden">
+
+        {/* Main content: Hero and others components inside <main></main> */}
+        <main>
+
+        </main>
+
+        {/* Footer <footer></footer> */}
+      </div>
+    </div>
+  );
+}
+
+export default landingNew;
