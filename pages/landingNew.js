627e7c4fa00fcacbb83777ab2aa6fb4c34e1087f
Maxime TFT <mkubik@tftlabs.com>
2022-02-18 20:44:42 +0100

    add: header_layout wip

--- pages/landingNew.js
+++ pages/landingNew.js
@@ -1,7 +1,10 @@
+import Navbar from "../components/landing-v2/Navbar";
+
 const landingNew = () => {
   return (
     <div className="bg-white">
       <div className="relative overflow-hidden">
+        <Navbar />
 
         {/* Main content: Hero and others components inside <main></main> */}
         <main>
