b2110b3e225ea7d2dcb686dd5bfd8ac6d50803f9
Maxime TFT <mkubik@tftlabs.com>
2022-02-23 15:24:45 +0100

    💄 fix: add margin left to add space beetween title and artwork

--- components/Hero.js
+++ components/Hero.js
@@ -4,7 +4,7 @@ const style = {
   wrapper: `relative`,
   container: `before:content-[''] before:bg-red-500 before:absolute before:top-0 before:left-0 before:right-0 before:bottom-0 before:bg-[#FFFFFF] before:bg-cover before:bg-center before:opacity-30 before:blur`,
   contentWrapper: `flex h-screen relative justify-center flex-wrap items-center`,
-  copyContainer: `w-1/2`,
+  copyContainer: `w-1/2 mr-5`,
   title: `relative text-[#000000] text-[46px] font-semibold`,
   description: `text-[#8a939b] container-[400px] text-2xl mt-[0.8rem] mb-[2.5rem]`,
   ctaContainer: `flex`,
